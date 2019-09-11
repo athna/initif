0f930fdfc267b4086722025b0284eb6f541eac8a
motya0414 <motya0414@hotmail.co.jp>
2019-09-11 15:05:11 +0900

    [update] get interface information from `ip a` command

--- ubuntu/initif_ubuntu.py
+++ ubuntu/initif_ubuntu.py
@@ -3,11 +3,25 @@
 
 import yaml
 import re
+import subprocess
 
 # YAML_PATH = "/etc/netplan/50-cloud-init.yaml"
 YAML_PATH = "50-cloud-init.yaml.org"
-interface_list = {}
+interface_list = []
 
+
+class Interface():
+    def __init__(self, int_number, int_name, mac_addr):
+        self.number = int_number
+        self.name = int_name
+        self.mac_addr = mac_addr
+
+    def info(self):
+        print("number:{}, name:{}, mac_addr:{}".format(self.number, self.name, self.mac_addr))
+
+
+## YAML
+'''
 with open(YAML_PATH) as stream:
     data = yaml.safe_load(stream)
     print(type(data))
@@ -15,13 +29,21 @@ with open(YAML_PATH) as stream:
 for key, value in data["network"]["ethernets"].items():
     print(key)
     print(value)
+'''
 
-with open("ip_a.txt", "r") as ipa:
-    ipa_data = ipa.readlines()
+## ip a command
+cmd = ("ip a")
+ipa_data = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True).stdout.readlines()
 
 is_skip = False
+temp_number = ""
+temp_int_name = ""
+temp_mac_addr = ""
+
 for data in ipa_data:
-    pattern = '\d:.*'
+    data = data.decode("utf-8")
+
+    pattern = r'\d:.*'
     result = re.match(pattern, data)
 
     if result:
@@ -33,12 +55,22 @@ for data in ipa_data:
     if is_skip:
         continue
 
-    pattern = '(?P<name>\d):\s*(?P<int_name>.*:).*'
+    pattern = r'\s*(?P<number>\d+):\s*(?P<int_name>.*):.*'
     result = re.match(pattern, data)
     if result:
-        print(result.groups())
+        temp_number = result.group("number")
+        temp_int_name = result.group("int_name")
 
-    pattern = 'link/ether\s*(?P<mac_addr>(\d | :)*)\s*.*'
-    result = re.match(pattern, data)
+    pattern = r"(link/ether|link/ieee802.11)\s*(?P<mac_addr>(\d|:|[a-f])*)\s.*"
+    result = re.search(pattern, data)
     if result:
-        print(result.groups())
+        temp_mac_addr = result.group("mac_addr")
+
+    if "" not in [temp_number, temp_int_name, temp_mac_addr]:
+        interface_list.append(Interface(temp_number, temp_int_name, temp_mac_addr))
+        temp_number = ""
+        temp_int_name = ""
+        temp_mac_addr = ""
+
+for interface in interface_list:
+    interface.info()
